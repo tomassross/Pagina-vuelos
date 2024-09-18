ee70594971386fa315be8b977b8ab41db3e7dce6
Tomás Ross <tom.rosscontacto@gmail.com>
2024-09-18 20:47:12 -0300

    Ultimos detalles

--- components/header/header-desktop.js
+++ components/header/header-desktop.js
@@ -2,13 +2,17 @@ class HeaderDesktopComponent extends HTMLElement {
     connectedCallback() {
       this.innerHTML = 
       `<nav id="nav">
-          <h1><img src="assets/Recurso 5.svg" alt="logo" id="logo"></h1>
-          <ul class="nav-list">
-              <li><a href="">Ofertas</a></li>
-              <li><a href="">Destinos de Moda</a></li>
-              <li><a href="">Favoritos</a></li>
-          </ul>
-       /nav>`;
+                <h1><a href="index.html"><img src="assets/Recurso 5.svg" alt="logo" id="logo"></a></h1>
+                <ul class="nav-list">
+                    <li><a href="index.html#ofertas">Ofertas</a></li>
+                    <li><a href="index.html#destinos-de-moda">Destinos de Moda</a></li>
+                    <li><a href="#">Favoritos</a></li>
+                </ul>
+                <div class="registro">
+                    <button class="Log-in-btn">Log in</button>
+                    <button class="sign-in-btn">sign in</button>
+                </div>
+            </nav>`;
     }
   }
 
