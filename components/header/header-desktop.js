e94585e0611f2437bb6011aa1914a07170bf4630
Tomás Ross <tom.rosscontacto@gmail.com>
2024-09-18 12:40:28 -0300

    versión con fallos pero con la otra página

--- components/header/header-desktop.js
+++ components/header/header-desktop.js
@@ -1,14 +1,14 @@
 class HeaderDesktopComponent extends HTMLElement {
     connectedCallback() {
       this.innerHTML = 
-      `    <nav id="nav">
-              <h1><img src="assets/Recurso 5.svg" alt="logo" id="logo"></h1>
-              <ul class="nav-list">
-                  <li><a href="">Ofertas</a></li>
-                  <li><a href="">Destinos de Moda</a></li>
-                  <li><a href="">Favoritos</a></li>
-              </ul>
-          </nav>`;
+      `<nav id="nav">
+          <h1><img src="assets/Recurso 5.svg" alt="logo" id="logo"></h1>
+          <ul class="nav-list">
+              <li><a href="">Ofertas</a></li>
+              <li><a href="">Destinos de Moda</a></li>
+              <li><a href="">Favoritos</a></li>
+          </ul>
+       /nav>`;
     }
   }
 
