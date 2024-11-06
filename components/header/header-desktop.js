558bb501976848b5e32f3d1134cae0acb4b88053
Tomás Ross <tom.rosscontacto@gmail.com>
2024-11-06 11:14:31 -0300

    Empezado página ofertas code

--- components/header/header-desktop.js
+++ components/header/header-desktop.js
@@ -4,7 +4,7 @@ class HeaderDesktopComponent extends HTMLElement {
       `<nav id="nav">
                 <h1><a href="index.html"><img src="assets/Recurso 5.svg" alt="logo" id="logo"></a></h1>
                 <ul class="nav-list">
-                    <li><a href="index.html#ofertas">Ofertas</a></li>
+                    <li><a href="ofertas.html">Ofertas</a></li>
                     <li><a href="index.html#destinos-de-moda">Destinos de Moda</a></li>
                     <li><a href="#">Favoritos</a></li>
                 </ul>
