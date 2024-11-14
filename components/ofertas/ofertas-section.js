817f7be59c15847d97f89442a77a7d4383884414
Tomás Ross <tom.rosscontacto@gmail.com>
2024-11-13 23:18:23 -0300

    componente ofertas en index

--- components/ofertas/ofertas-section.js
+++ components/ofertas/ofertas-section.js
@@ -3,9 +3,18 @@ class OfertasComponent extends HTMLElement {
     connectedCallback() {
       this.innerHTML = 
       `<section id="ofertas">
-          
-      </section>`;
-    }
+            <div class="ofertas-content">
+                <h2>Ofertas</h2>
+                <p>Promociones, descuentos y ofertas especiales para ti</p>
+                <div class="ofertas-container">
+                    <div class="overlay-ofertas"></div>
+                    <a href="ofertas.html">
+                        <button>Ver ofertas</button>
+                    </a>
+                </div>
+            </div>
+        </section>`;
   }
+}
 
 customElements.define('ofertas-section', OfertasComponent);
